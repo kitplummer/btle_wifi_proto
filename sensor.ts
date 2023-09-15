adafe0f7dab13264d55e97d8e29bdbfa3d2b786b
Kit Plummer <kit@ditto.live>
2023-09-15 12:13:18 -0400

    well, trying a sleep to get the pixel lit

--- sensor.ts
+++ sensor.ts
@@ -99,14 +99,16 @@ async function main() {
 
   rawSub = ditto.store.collection("raw_data").findAll().subscribe()
   rawLiveQuery = ditto.store.collection("raw_data").findAll().observeLocal((doc, event) => {
-    sense.setPixel(6, 7, black)
-    sense.setPixel(6, 7, blue)
-    sense.setPixel(6, 7, red)
-    sleep(500)
-    sense.setPixel(6, 7, blue)
-    sense.setPixel(6, 7, red)
-    sleep(500)
-    sense.setPixel(6, 7, black)
+    for (i = 0; i < 10; i++) {
+      sense.setPixel(6, 7, black)
+      sense.setPixel(6, 7, blue)
+      sense.setPixel(6, 7, red)
+      sleep(500)
+      sense.setPixel(6, 7, blue)
+      sense.setPixel(6, 7, red)
+      sleep(500)
+      sense.setPixel(6, 7, black)
+    }
     Logger.info(`raw data received`)
   })
 
